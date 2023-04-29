bb31e83a45de84c8816e26698b916a9de0a5fc20
Maks Subochev <maxim.subochev@yandex.ru>
2023-04-29 12:40:07 +0300

    fix: добавить липкий футер, фавикон

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,36 @@
-function Card({card, onCardClick}) {
+function Card({ card, onCardClick }) {
+  const handleCardClick = () => {
+    onCardClick(card);
+  };
 
-    const handleCardClick = () => {
-        onCardClick(card);
-    };
-
-    return (
-        <li className="places__item">
-                <figure className="places__image-wrapper">
-                  <img 
-                  src={card.link}
-                  alt={card.name}
-                  className="places__image"
-                  onClick={handleCardClick}
-                  />
-                  <figcaption className="places__discription">
-                    <p className="places__text">{card.name}</p>
-                    <div className="places__wrapper-like">
-                      <button
-                      type="button"
-                      className="places__button-like"
-                      aria-label="кнопка лайка"
-                    ></button>
-                    <p className="places__like-counter">{card.likes.length}</p>
-                  </div>
-                  </figcaption>
-                  <button
-                    type="button"
-                    className="places__button-trash"
-                    aria-label="кнопка удаления"
-                  ></button>
-                </figure>
-              </li>
-    );
+  return (
+    <li className="places__item">
+      <figure className="places__image-wrapper">
+        <img
+          src={card.link}
+          alt={card.name}
+          className="places__image"
+          onClick={handleCardClick}
+        />
+        <figcaption className="places__discription">
+          <p className="places__text">{card.name}</p>
+          <div className="places__wrapper-like">
+            <button
+              type="button"
+              className="places__button-like"
+              aria-label="кнопка лайка"
+            ></button>
+            <p className="places__like-counter">{card.likes.length}</p>
+          </div>
+        </figcaption>
+        <button
+          type="button"
+          className="places__button-trash"
+          aria-label="кнопка удаления"
+        ></button>
+      </figure>
+    </li>
+  );
 }
 
 export default Card;
